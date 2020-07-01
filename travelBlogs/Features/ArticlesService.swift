9b377bb72007eace315018c2037e6f3bf8e4452d
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-01 21:47:09 +0530

    Added NetworkRequest and ArticleService for consuming Article Endpoint.
    (cherry picked from commit 9e1d9c625201fdaa0367c7333a829aca41d91eef)

---
+++ travelBlogs/Features/ArticlesService.swift
@@ -0,0 +1,27 @@
+//
+//  ArticlesService.swift
+//  travelBlogs
+//
+//  Created by Ganesh Somani on 01/07/20.
+//  Copyright © 2020 Ganesh Somani. All rights reserved.
+//
+
+import Foundation
+
+struct ArticlesService: NetworkRequest {
+    
+    typealias ModelType = [ArticleDataModel]
+    
+    func loadArticles(endpoint: APIEndpoint, completion: @escaping ([ArticleDataModel]?, Error?) -> Void) {
+        guard let request = endpoint.request else {
+            completion(nil, NSError())
+            return
+        }
+        self.load(request, withCompletion: completion)
+    }
+    
+    func decode(_ data: Data) -> [ArticleDataModel]? {
+        let articles = try? JSONDecoder().decode([ArticleDataModel].self, from: data)
+        return articles
+    }
+}
---
+++ travelBlogs/Network/NetworkRequest.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkRequest.swift
+//  travelBlogs
+//
+//  Created by Ganesh Somani on 01/07/20.
+//  Copyright © 2020 Ganesh Somani. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkRequest {
+    
+    associatedtype ModelType
+        
+    /// Decodes the data fetched via API call
+    ///
+    /// - parameter data: The dato be decoded
+    /// - returns: A `Decodable`, or nil if decoding fails.
+    func decode(_ data: Data) -> ModelType?
+    
+    
+    /// Performs a load operation to fetch data from network
+    func load(_ request: URLRequest, withCompletion completion: @escaping (ModelType?, Error?) -> Void)
+}
+
+extension NetworkRequest {
+    
+    func load(_ request: URLRequest, withCompletion completion: @escaping (ModelType?, Error?) -> Void) {
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            guard let data = data else {
+                completion(nil, NSError())
+                return
+            }
+            guard let decodedData = self.decode(data) else {
+                completion(nil, NSError())
+                return
+            }
+            completion(decodedData, nil)
+        })
+        task.resume()
+    }
+}
