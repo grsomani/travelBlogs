41b97c0416c0dbd647f02228d636bb19b9e439ae
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-01 19:13:01 +0530

    Empty project with gitignore

---
+++ travelBlogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travelBlogs
+//
+//  Created by Ganesh Somani on 01/07/20.
+//  Copyright © 2020 Ganesh Somani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
