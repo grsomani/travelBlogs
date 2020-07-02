7cae4e89e4c00ac6e40f29f1cc654acbbd708f4a
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-02 11:43:09 +0530

    Tested Data Model for parsing.
    (cherry picked from commit f899fe732eb554b9dbb1d4362269d550966858af)

--- travelBlogs/Features/Articles/Data Model/ArticleDataModel.swift
+++ travelBlogs/Features/Articles/Data Model/ArticleDataModel.swift
@@ -9,13 +9,6 @@
 import Foundation
 
 
-//  "id": "1",
-//  "createdAt": "2020-04-17T12:13:44.575Z",
-//  "content": "calculating the program won't do anything, we need to navigate the multi-byte SMS alarm!",
-//  "comments": 8237,
-//  "likes": 62648,
-//  "media": [],
-//  "user": []
 struct ArticleDataModel: Codable {
     let id, createdAt, content: String?
     let comments, likes: Int?
@@ -23,12 +16,6 @@ struct ArticleDataModel: Codable {
     let user: [User]?
 }
 
-//  "id": "1",
-//  "blogId": "1",
-//  "createdAt": "2020-04-16T22:43:18.606Z",
-//  "image": "https://s3.amazonaws.com/uifaces/faces/twitter/joe_black/128.jpg",
-//  "title": "maximized system",
-//  "url": "http://providenci.com"
 struct Media: Codable {
     let id, blogID, createdAt: String?
     let image: String?
@@ -41,15 +28,6 @@ struct Media: Codable {
     }
 }
 
-//  "id": "1",
-//  "blogId": "1",
-//  "createdAt": "2020-04-16T20:17:42.437Z",
-//  "name": "Dayton",
-//  "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/erwanhesry/128.jpg",
-//  "lastname": "Haag",
-//  "city": "West Ima",
-//  "designation": "Human Group Assistant",
-//  "about": "Try to calculate the SDD bandwidth, maybe it will override the auxiliary card!"
 struct User: Codable {
     let id, blogID, createdAt, name: String
     let avatar: String
