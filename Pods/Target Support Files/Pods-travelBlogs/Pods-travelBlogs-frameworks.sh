ed0cd28c3dbcd669832c72f719fb2c2ea31aca60
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-02 14:25:57 +0530

    Installed pods for progress view and reachability

--- Pods/Target Support Files/Pods-travelBlogs/Pods-travelBlogs-frameworks.sh
+++ Pods/Target Support Files/Pods-travelBlogs/Pods-travelBlogs-frameworks.sh
@@ -161,10 +161,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MKProgress/MKProgress.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MKProgress/MKProgress.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/OHHTTPStubs/OHHTTPStubs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
