cc711eda541d392d34c1593f1059e10281928848
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-04 00:12:59 +0530

    Added CoreData files

--- travelBlogs/AppDelegate/AppDelegate.swift
+++ travelBlogs/AppDelegate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +22,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         window?.rootViewController = tabBarController
         return true
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "travelBlogs")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
 
