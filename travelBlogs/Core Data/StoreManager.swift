77e8203415df4a3bedfd86ce64e7e96087a55cad
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-04 01:12:50 +0530

    Added store manager

---
+++ travelBlogs/Core Data/StoreManager.swift
@@ -0,0 +1,33 @@
+//
+//  StoreManager.swift
+//  travelBlogs
+//
+//  Created by Ganesh Somani on 04/07/20.
+//  Copyright © 2020 Ganesh Somani. All rights reserved.
+//
+
+import Foundation
+
+class StoreManager {
+    
+    static var persistentStoreCoordinator: NSPersistentStoreCoordinator? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.persistentStoreCoordinator
+        }
+        return nil
+    }
+    
+    static var managedObjectModel: NSManagedObjectModel? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.managedObjectModel
+        }
+        return nil
+    }
+    
+    static var managedObjectContext: NSManagedObjectContext? {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            return appDelegate.persistentContainer.viewContext
+        }
+        return nil
+    }
+}
