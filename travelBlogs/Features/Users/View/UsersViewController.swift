b8f5c4c27553ccac12cb19156faa0ca334232e20
Ganesh Somani <ganeshsomani@hotmail.com>
2020-07-03 23:57:01 +0530

    Added Empty users view controller

---
+++ travelBlogs/Features/Users/View/UsersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UsersViewController.swift
+//  travelBlogs
+//
+//  Created by Ganesh Somani on 03/07/20.
+//  Copyright © 2020 Ganesh Somani. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Users"
+    }
+
+}
